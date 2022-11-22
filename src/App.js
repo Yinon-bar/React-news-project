a5677a3b7e0645aa5312aaf821278b9360a7698e
Yinon Bar <yinonbar1988@gmail.com>
2022-11-22 22:47:33 +0200

    clean slate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./Components/Layout/Header/Header";
+import Main from "./Components/Layout/Main/Main";
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Layout/Header/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="Container">
+        <h1>Header</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
